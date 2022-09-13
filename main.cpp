994325d94dc313fd1d3f91124f349d2a534ca19a
luboshnikov <luboshnikovm@gmail.com>
2022-09-13 23:54:56 +0200

    class Randomize

---
+++ main.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <unordered_map>
+#include <vector>
+
+using namespace std;
+
+class Randomize {
+public:
+    bool insert(int val){
+        if(map.find(val) != map.end()){
+            return false;
+        }
+        int id = arr.size();
+        arr.push_back(val);
+        map[val] = id;
+    }
+    bool remove(int val){
+        if(map.find(val) == map.end()){
+            return false;
+        }
+        int id = map[val];
+        map.erase(val);
+        arr[id] = arr.back();
+        map[arr[id]] = id;
+        arr.pop_back();
+    }
+    bool find(const int val){
+        return map.find(val) != map.end();
+
+    }
+    int get_random(){
+        int id = rand() % arr.size();
+        return arr[id];
+    }
+private:
+    vector<int> arr;
+    unordered_map<int, int> map;
+};
